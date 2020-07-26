481973db4122e10e2b0ddfc009622a20dd82edb7
Omama Zainab <omi.zainy@gmail.com>
2020-07-26 11:43:14 +0500

    flip cards updated

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,24 +1,63 @@
 import React from 'react'
-// import Right from '../images/rightarrow.png'
+import Right from '../images/nclab.png'
 import Left from '../images/leftarrow.png'
 
 const MyWork = () => {
+
+    // document.getElementById("card1").addEventListener("mouseover", function (event) {
+    //     console.log('called on hover'); //just to make sure the function is getting called   
+
+
+    // }, false);
+
     return (
         <div className="container mx-auto my-5 py-5" id="myWork" >
             <div className="row my-5">
-                <div className="col-md-3 mx-auto my-5 project-card" id="card1" data-aos="zoom-in-right">
-                    
+
+                <div className="col-md-3 mx-auto" id="" data-aos="zoom-in-right">
+
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={Right} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://nclab.neduet.edu.pk/" style={{ color: 'black' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="col-md-3  mx-auto my-5 project-card" id="card2" data-aos="zoom-in">
-                    
+
+                <div className="col-md-3  mx-auto" id="" data-aos="zoom-in">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={Right} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://nclab.neduet.edu.pk/" style={{ color: 'black' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-3  mx-auto my-5 project-card" id="card3" data-aos="zoom-in-left">
-                    
+
+                <div className="col-md-3 mx-auto " id="" data-aos="zoom-in-left">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={Right} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://nclab.neduet.edu.pk/" style={{ color: 'black' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="row">
                 <a href="#aboutme" className="mx-auto">
-                    <img src={Left} alt="back"  />
+                    <img src={Left} alt="back" />
                 </a>
                 {/* <a href="#" className="mx-auto">
                     <img src={Right} alt="forward" />
