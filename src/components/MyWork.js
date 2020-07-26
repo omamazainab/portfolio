4dec44df8e32136101cce5f0a2fa02be1036ee18
Omama Zainab <omi.zainy@gmail.com>
2020-07-26 16:37:18 +0500

    my work

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,5 +1,12 @@
 import React from 'react'
-import Right from '../images/nclab.png'
+import NCLAB from '../images/nclab.png'
+import ShoeShop from '../images/shoeshop101.png'
+import STYLEEE from '../images/styleee.png'
+import ExpenseTracker from '../images/expensetracker.png'
+import TraCOVID from '../images/TraCOVID.png'
+import FlexPanel from '../images/flexpanel.png'
+import muscicKit from '../images/music kit.png'
+import RedQueen from '../images/redQueen.png'
 import Left from '../images/leftarrow.png'
 
 const MyWork = () => {
@@ -14,42 +21,130 @@ const MyWork = () => {
         <div className="container mx-auto my-5 py-5" id="myWork" >
             <div className="row my-5">
 
-                <div className="col-md-3 mx-auto" id="" data-aos="zoom-in-right">
+                <div className="col-lg-3 mx-auto" id="" data-aos="zoom-in-right">
 
                     <div class="flip-card">
                         <div class="flip-card-inner">
                             <div class="flip-card-front">
-                                <img src={Right} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                                <img src={NCLAB} alt="Avatar" style={{ width: '300px', height: '300px' }} />
                             </div>
                             <div class="flip-card-back">
-                                <a href="https://nclab.neduet.edu.pk/" style={{ color: 'black' }}>view website</a>
+                                <a href="https://nclab.neduet.edu.pk/" style={{ color: 'white' }}>view website</a>
                             </div>
                         </div>
                     </div>
 
                 </div>
 
-                <div className="col-md-3  mx-auto" id="" data-aos="zoom-in">
+                <div className="col-lg-3  mx-auto" id="" data-aos="zoom-in">
                     <div class="flip-card">
                         <div class="flip-card-inner">
                             <div class="flip-card-front">
-                                <img src={Right} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                                <img src={ShoeShop} alt="Avatar" style={{ width: '300px', height: '300px' }} />
                             </div>
                             <div class="flip-card-back">
-                                <a href="https://nclab.neduet.edu.pk/" style={{ color: 'black' }}>view website</a>
+                                <a href="http://shoeshop101.surge.sh/" style={{ color: 'white' }}>view website</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col-md-3 mx-auto " id="" data-aos="zoom-in-left">
+                <div className="col-lg-3 mx-auto " id="" data-aos="zoom-in-left">
                     <div class="flip-card">
                         <div class="flip-card-inner">
                             <div class="flip-card-front">
-                                <img src={Right} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                                <img src={STYLEEE} alt="Avatar" style={{ width: '300px', height: '300px' }} />
                             </div>
                             <div class="flip-card-back">
-                                <a href="https://nclab.neduet.edu.pk/" style={{ color: 'black' }}>view website</a>
+                                <a href="https://styleee-style-forward.web.app/" style={{ color: 'white' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row my-5">
+
+                <div className="col-lg-3 mx-auto" id="" data-aos="zoom-in-right">
+
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={ExpenseTracker} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="http://expense-tracker-app-omama.surge.sh/" style={{ color: 'white' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col-lg-3  mx-auto" id="" data-aos="zoom-in">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={TraCOVID} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="http://shoeshop101.surge.sh/" style={{ color: 'white' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 mx-auto " id="" data-aos="zoom-in-left">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={muscicKit} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://js-and-css-music-kit.web.app/" style={{ color: 'white' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row my-5">
+
+                <div className="col-lg-3 mx-auto" id="" data-aos="zoom-in-right">
+
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={RedQueen} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="http://red-queen_race-omamazainab.surge.sh/" style={{ color: 'white' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                {/* <div className="col-lg-3  mx-auto" id="" data-aos="zoom-in">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={TraCOVID} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="http://shoeshop101.surge.sh/" style={{ color: 'white' }}>view website</a>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+
+                <div className="col-lg-3 mx-auto " id="" data-aos="zoom-in-left">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={FlexPanel} alt="Avatar" style={{ width: '300px', height: '300px' }} />
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://flex-panels-image-galler-58b1c.web.app" style={{ color: 'white' }}>view website</a>
                             </div>
                         </div>
                     </div>
